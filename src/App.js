531d24cd42aae36a93cec76e5a3f3f417204a568
Ashwin <8831182+ashwinr63@users.noreply.github.com>
2022-05-17 01:10:09 -0400

    Adding Functional Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,71 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
-class App extends Component {
+
+const App = () => {
+
+  const [searchField, setSearchField] = useState(''); // value, setValue
+  const [monsters, setMonsters] = useState([]); 
+  const[filteredMonsters, setFilteredMonsters] = useState(monsters);
+  const [stringField, setStringField] = useState('');
+
+  console.log('render')
+
+  
+  useEffect(() => {
+    console.log('effect fired')
+  fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setMonsters(users))
+  }, [])
+      
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    })
+
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField])
+
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+
+  }
+  const onStringChange = (event) => {
+    setStringField(event.target.value);
+  }
+
+ 
+
+  return (
+    <div className='App'>
+      <h1 className='app-title'>
+        Rolodex
+      </h1>
+
+      <SearchBox
+        className='search-box'
+        onChangeHandler={onSearchChange}
+        placeholder='Search a Monster'
+      />
+       <SearchBox
+        onChangeHandler={onStringChange}
+        placeholder='setString'
+      />
+
+    <CardList monsters={filteredMonsters} />
+    </div>
+
+  )
+
+}
+
+/* class App extends Component {
 
   constructor() {
     super();
@@ -61,6 +122,6 @@ class App extends Component {
       </div>
     );
   }
-}
+} */
 
 export default App;
